5e70cf716eb20d767a63a3068a9b48a9fb5e0338
Rodolfo Roca <rroca1982@gmail.com>
2018-12-10 21:39:51 -0300

    - Implemented JSON and URL parameter encoders - Added RestKitError enum

---
+++ RestKit/Enums/RestKitError.swift
@@ -0,0 +1,21 @@
+//
+//  RestKitError.swift
+//  RestKit
+//
+//  Created by Rodolfo Roca on 12/10/18.
+//  Copyright © 2018 Rodolfo Roca. All rights reserved.
+//
+
+import Foundation
+
+enum RestKitError: Error {
+    case parametersNil
+    case encodingFailed
+    case decodingFailed
+    case missingURL
+    case wrongURL
+    case lostConnection
+    case notFound
+    case saveError
+    case unknownError
+}
